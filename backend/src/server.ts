cfa42d167c57ab08e1f6c20c21fa900e5f37417f
chitraguptast <chitragupta.st@gmail.com>
2025-11-29 22:34:46 +0530

    whatever you changed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,10 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./utils/db"; // <-- YOU FORGOT TO RUN THIS
 import { recipeRouter } from "./routes/recipeRoutes";
+import path from "path";
+
+
+
 
 const app = express();
 
@@ -14,6 +18,11 @@ connectDB();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 app.use("/api/recipes", recipeRouter);
 const PORT = process.env.PORT || 5000;
