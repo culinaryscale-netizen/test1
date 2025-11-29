b478a3ca886bd8c3502a2f856f4fa4fa7dc48f56
chitraguptast <chitragupta.st@gmail.com>
2025-11-29 22:54:54 +0530

    whatever you changed3

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export async function getAllRecipes() {
   const res = await fetch(`${API_URL}/api/recipes`);
